96d6d63b82a0b8c221254daa0d654f35cca88034
xandwch <itsmethesandwichman@gmail.com>
2023-03-20 20:39:56 -0500

    Added new textures to tiles.

--- GodotRumbleProject/data.gd
+++ GodotRumbleProject/data.gd
@@ -1,8 +1,12 @@
 extends Node
 
+enum{
+	SAFE,
+	DANGER
+}
 
 var player : Node
 var map : Node
 var label : Label
 
-var state_count = 2
+var state_count = 4
--- GodotRumbleProject/tile.gd
+++ GodotRumbleProject/tile.gd
@@ -3,29 +3,38 @@ extends Node2D
 var safe_image = preload("res://filler_art/filler_safe_tile.png")
 var hole_image = preload("res://filler_art/filler_hole_tile.png")
 
-var texture
+@onready var animatedSprite
 
+var tile_change_time
+
+var animation
+var texture
 var sprite 
 
 var state = 0
 var map_pos : Vector2
-
-func set_state(new_state):
-	state = new_state
-	if new_state == 0:
-		if sprite is Sprite2D:
-			sprite.texture = safe_image
-		texture = safe_image
-	elif new_state == 1:
-		if sprite is Sprite2D:
-			sprite.texture = hole_image
-		texture = hole_image
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	sprite = get_node("Sprite2D")
 	sprite.texture = texture
 
+
+func set_state(new_state):
+	animatedSprite = $AnimatedSprite2D
+	var last_state = state
+	state = new_state
+	match last_state:
+		Data.SAFE:
+			animatedSprite.play("Green_Safe_Flashing")
+			
+	match state:
+		Data.SAFE:
+			animatedSprite.play("Green_Safe_Static")
+		Data.DANGER:
+			animatedSprite.play("Red_Danger_Static")
+		_:
+			pass
 
 func random_state():
 	randomize()
@@ -41,3 +50,7 @@ func end_effect():
 	
 func end_effect_of_state(s):
 	pass
+
+
+func _on_timer_timeout():
+	
